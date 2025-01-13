55f20dc6debb347c61312aba4231829a2ea377dd
Keru1712 <133781338+Keru1712@users.noreply.github.com>
2025-01-13 23:12:08 +0700

    fix: ckeditor ads edit-blog

--- public/js/ckeditor.js
+++ public/js/ckeditor.js
@@ -1,15 +1,128 @@
 const {
-    ClassicEditor,
+  ClassicEditor,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  BalloonToolbar,
+  BlockQuote,
+  BlockToolbar,
+  Bold,
+  Bookmark,
+  Code,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  MediaEmbed,
+  Mention,
+  PageBreak,
+  Paragraph,
+  PasteFromOffice,
+  RemoveFormat,
+  ShowBlocks,
+  SimpleUploadAdapter,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCaption,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextPartLanguage,
+  TextTransformation,
+  Title,
+  TodoList,
+  Underline,
+  WordCount
+} = window.CKEDITOR;
+
+const LICENSE_KEY =
+  'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Mzc1OTAzOTksImp0aSI6IjZkNDc3NGZmLWQ5MmUtNGRlYS05ZjZlLTc4ZDBkYTViMzBhZCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiLCJzaCJdLCJ3aGl0ZUxhYmVsIjp0cnVlLCJsaWNlbnNlVHlwZSI6InRyaWFsIiwiZmVhdHVyZXMiOlsiKiJdLCJ2YyI6IjIzNzQ1NjIzIn0.rVBzN-wdX6cbTP241gdaV6JTAb1QTPHOVfJHhhdF_s65Kyt2ROvaYVHaT3yLrLxD0j1HInkpfvsAFB7lynowHg';
+
+const editorConfig = {
+  toolbar: {
+    items: [
+      'sourceEditing',
+      'showBlocks',
+      '|',
+      'heading',
+      'style',
+      '|',
+      'fontSize',
+      'fontFamily',
+      'fontColor',
+      'fontBackgroundColor',
+      '|',
+      'bold',
+      'italic',
+      'underline',
+      '|',
+      'link',
+      'insertImage',
+      'insertTable',
+      'highlight',
+      'blockQuote',
+      '|',
+      'alignment',
+      '|',
+      'bulletedList',
+      'numberedList',
+      'todoList',
+      'outdent',
+      'indent'
+    ],
+    shouldNotGroupWhenFull: false
+  },
+  plugins: [
+    Alignment,
+    Autoformat,
     AutoImage,
+    AutoLink,
     Autosave,
     BalloonToolbar,
     BlockQuote,
     BlockToolbar,
     Bold,
-    CloudServices,
+    Bookmark,
     Code,
-    CodeBlock,
     Essentials,
+    FindAndReplace,
     FontBackgroundColor,
     FontColor,
     FontFamily,
@@ -17,8 +130,11 @@ const {
     GeneralHtmlSupport,
     Heading,
     Highlight,
+    HorizontalLine,
     ImageBlock,
+    ImageCaption,
     ImageInline,
+    ImageInsert,
     ImageInsertViaUrl,
     ImageResize,
     ImageStyle,
@@ -32,223 +148,238 @@ const {
     LinkImage,
     List,
     ListProperties,
+    MediaEmbed,
+    Mention,
+    PageBreak,
     Paragraph,
+    PasteFromOffice,
+    RemoveFormat,
+    ShowBlocks,
+    SimpleUploadAdapter,
     SourceEditing,
+    SpecialCharacters,
+    SpecialCharactersArrows,
+    SpecialCharactersCurrency,
+    SpecialCharactersEssentials,
+    SpecialCharactersLatin,
+    SpecialCharactersMathematical,
+    SpecialCharactersText,
     Strikethrough,
+    Style,
+    Subscript,
+    Superscript,
     Table,
     TableCaption,
     TableCellProperties,
     TableColumnResize,
     TableProperties,
     TableToolbar,
+    TextPartLanguage,
+    TextTransformation,
+    Title,
     TodoList,
     Underline,
-    SimpleUploadAdapter
-  } = window.CKEDITOR;
-  
-  const LICENSE_KEY =
-    'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3Njc5MTY3OTksImp0aSI6ImViMWZkYjc3LTViZjQtNGVjZS1hYzlhLTc1ODc3MzcyMjIxYSIsImxpY2Vuc2VkSG9zdHMiOlsiMTI3LjAuMC4xIiwibG9jYWxob3N0IiwiMTkyLjE2OC4qLioiLCIxMC4qLiouKiIsIjE3Mi4qLiouKiIsIioudGVzdCIsIioubG9jYWxob3N0IiwiKi5sb2NhbCJdLCJ1c2FnZUVuZHBvaW50IjoiaHR0cHM6Ly9wcm94eS1ldmVudC5ja2VkaXRvci5jb20iLCJkaXN0cmlidXRpb25DaGFubmVsIjpbImNsb3VkIiwiZHJ1cGFsIl0sImxpY2Vuc2VUeXBlIjoiZGV2ZWxvcG1lbnQiLCJmZWF0dXJlcyI6WyJEUlVQIl0sInZjIjoiZmZkMDRiZDkifQ.oj0BhEJcz_KolJGVpy1vsbymvGW5tCiR5jMyVIYL9UpERJQ3dSrPOo15jJLk2rh0TUX3l9nLqolyHcqftoDmdg';
-  
-  const editorConfig = {
-    toolbar: {
-      items: [
-        'undo',
-        'redo',
-        'sourceEditing',
-        '|',
-        'heading',
-        '|',
-        'insertImage',
-        'code',
-        '|',
-        'fontSize',
-        'fontFamily',
-        'fontColor',
-        'fontBackgroundColor',
-        '|',
-        'bold',
-        'italic',
-        'underline',
-        'strikethrough',
-        '|',
-        'link',
-        'insertTable',
-        'highlight',
-        'blockQuote',
-        'codeBlock',
-        '|',
-        'bulletedList',
-        'numberedList',
-        'todoList',
-        'outdent',
-        'indent'
-      ],
-      shouldNotGroupWhenFull: false
-    },
-    plugins: [
-      AutoImage,
-      Autosave,
-      BalloonToolbar,
-      BlockQuote,
-      BlockToolbar,
-      Bold,
-      CloudServices,
-      Code,
-      CodeBlock,
-      Essentials,
-      FontBackgroundColor,
-      FontColor,
-      FontFamily,
-      FontSize,
-      GeneralHtmlSupport,
-      Heading,
-      Highlight,
-      ImageBlock,
-      ImageInline,
-      ImageInsertViaUrl,
-      ImageResize,
-      ImageStyle,
-      ImageTextAlternative,
-      ImageToolbar,
-      ImageUpload,
-      Indent,
-      IndentBlock,
-      Italic,
-      Link,
-      LinkImage,
-      List,
-      ListProperties,
-      Paragraph,
-      SourceEditing,
-      Strikethrough,
-      Table,
-      TableCaption,
-      TableCellProperties,
-      TableColumnResize,
-      TableProperties,
-      TableToolbar,
-      TodoList,
-      Underline,
-      SimpleUploadAdapter
-    ],
-    balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
-    blockToolbar: ['bold', 'italic', '|', 'link', 'insertImage', 'insertTable', '|', 'bulletedList', 'numberedList', 'outdent', 'indent'],
-    fontFamily: {
-      options: [
-        'Inter, sans-serif',
-        'Arial, Helvetica, sans-serif',
-        'Courier New, Courier, monospace',
-        'Georgia, serif',
-        'Lucida Sans Unicode, Lucida Grande, sans-serif',
-        'Tahoma, Geneva, sans-serif',
-        'Times New Roman, Times, serif',
-        'Trebuchet MS, Helvetica, sans-serif',
-        'Verdana, Geneva, sans-serif'
-      ],
-      supportAllValues: true,
-      default: 'Inter, Arial, Helvetica, sans-serif'
-    },
-    fontSize: {
-      options: [10, 12, 14, 'default', 18, 20, 22],
-      supportAllValues: true
-    },
-    inlineStyles: true,
-    heading: {
-      options: [
-        {
-          model: 'paragraph',
-          title: 'Paragraph',
-          class: 'ck-heading_paragraph'
-        },
-        {
-          model: 'heading1',
-          view: 'h1',
-          title: 'Heading 1',
-          class: 'ck-heading_heading1'
-        },
-        {
-          model: 'heading2',
-          view: 'h2',
-          title: 'Heading 2',
-          class: 'ck-heading_heading2'
-        },
-        {
-          model: 'heading3',
-          view: 'h3',
-          title: 'Heading 3',
-          class: 'ck-heading_heading3'
-        },
-        {
-          model: 'heading4',
-          view: 'h4',
-          title: 'Heading 4',
-          class: 'ck-heading_heading4'
-        },
-        {
-          model: 'heading5',
-          view: 'h5',
-          title: 'Heading 5',
-          class: 'ck-heading_heading5'
-        },
-        {
-          model: 'heading6',
-          view: 'h6',
-          title: 'Heading 6',
-          class: 'ck-heading_heading6'
-        }
-      ]
-    },
-    htmlSupport: {
-      allow: [
-        {
-          name: /^.*$/,
-          styles: true,
-          attributes: true,
-          classes: true
-        }
-      ]
-    },
-    image: {
-      toolbar: ['imageTextAlternative', '|', 'imageStyle:inline', 'imageStyle:wrapText', 'imageStyle:breakText', '|', 'resizeImage']
-    },
-    simpleUpload: {
-      uploadUrl: '/upload',
-    },
-    initialData:
-      '<h2 style="font-family: Arial, Helvetica, sans-serif; font-size: 18px;">Your new blog here 🎉</h2>',
-    licenseKey: LICENSE_KEY,
-    link: {
-      addTargetToExternalLinks: true,
-      defaultProtocol: 'https://',
-      decorators: {
-        toggleDownloadable: {
-          mode: 'manual',
-          label: 'Downloadable',
-          attributes: {
-            download: 'file'
-          }
-        }
+    WordCount
+  ],
+  balloonToolbar: ['bold', 'italic', '|', 'link', 'insertImage', '|', 'bulletedList', 'numberedList'],
+  blockToolbar: [
+    'fontSize',
+    'fontColor',
+    'fontBackgroundColor',
+    '|',
+    'bold',
+    'italic',
+    '|',
+    'link',
+    'insertImage',
+    'insertTable',
+    '|',
+    'bulletedList',
+    'numberedList',
+    'outdent',
+    'indent'
+  ],
+  fontFamily: {
+    supportAllValues: true
+  },
+  fontSize: {
+    options: [10, 12, 14, 'default', 18, 20, 22],
+    supportAllValues: true
+  },
+  heading: {
+    options: [
+      {
+        model: 'paragraph',
+        title: 'Paragraph',
+        class: 'ck-heading_paragraph'
+      },
+      {
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1'
+      },
+      {
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2'
+      },
+      {
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3'
+      },
+      {
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4',
+        class: 'ck-heading_heading4'
+      },
+      {
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5',
+        class: 'ck-heading_heading5'
+      },
+      {
+        model: 'heading6',
+        view: 'h6',
+        title: 'Heading 6',
+        class: 'ck-heading_heading6'
       }
-    },
-    list: {
-      properties: {
+    ]
+  },
+  htmlSupport: {
+    allow: [
+      {
+        name: /^.*$/,
         styles: true,
-        startIndex: true,
-        reversed: true
+        attributes: true,
+        classes: true
       }
-    },
-    menuBar: {
-      isVisible: true
-    },
-    placeholder: 'Type or paste your content here!',
-    table: {
-      contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+    ]
+  },
+  image: {
+    toolbar: [
+      'toggleImageCaption',
+      'imageTextAlternative',
+      '|',
+      'imageStyle:inline',
+      'imageStyle:wrapText',
+      'imageStyle:breakText',
+      '|',
+      'resizeImage'
+    ]
+  },
+  initialData:
+    '<h2 style="font-family: Arial, Helvetica, sans-serif; font-size: 18px;">Your new blog here 🎉</h2>',
+  licenseKey: LICENSE_KEY,
+  link: {
+    addTargetToExternalLinks: true,
+    defaultProtocol: 'https://',
+    decorators: {
+      toggleDownloadable: {
+        mode: 'manual',
+        label: 'Downloadable',
+        attributes: {
+          download: 'file'
+        }
+      }
     }
-  };
-  
-  function initializeCKEditor(selector) {
-    ClassicEditor.create(document.querySelector(selector), editorConfig)
-      .catch(error => {
-        console.error(error);
-      });
+  },
+  list: {
+    properties: {
+      styles: true,
+      startIndex: true,
+      reversed: true
+    }
+  },
+  mention: {
+    feeds: [
+      {
+        marker: '@',
+        feed: [
+          /* See: https://ckeditor.com/docs/ckeditor5/latest/features/mentions.html */
+        ]
+      }
+    ]
+  },
+  menuBar: {
+    isVisible: true
+  },
+  placeholder: 'Type or paste your content here!',
+  style: {
+    definitions: [
+      {
+        name: 'Article category',
+        element: 'h3',
+        classes: ['category']
+      },
+      {
+        name: 'Title',
+        element: 'h2',
+        classes: ['document-title']
+      },
+      {
+        name: 'Subtitle',
+        element: 'h3',
+        classes: ['document-subtitle']
+      },
+      {
+        name: 'Info box',
+        element: 'p',
+        classes: ['info-box']
+      },
+      {
+        name: 'Side quote',
+        element: 'blockquote',
+        classes: ['side-quote']
+      },
+      {
+        name: 'Marker',
+        element: 'span',
+        classes: ['marker']
+      },
+      {
+        name: 'Spoiler',
+        element: 'span',
+        classes: ['spoiler']
+      },
+      {
+        name: 'Code (dark)',
+        element: 'pre',
+        classes: ['fancy-code', 'fancy-code-dark']
+      },
+      {
+        name: 'Code (bright)',
+        element: 'pre',
+        classes: ['fancy-code', 'fancy-code-bright']
+      }
+    ]
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
   }
+};
+
+function initializeCKEditor(selector, blog) {
+  ClassicEditor.create(document.querySelector(selector), editorConfig)
+    .then(editor => {
+      // If blog is provided, set the editor data to the blog content
+      if (blog && blog.content) {
+        editor.setData(blog.content);
+      }
+
+      const wordCount = editor.plugins.get('WordCount');
+      document.querySelector('#editor-word-count').appendChild(wordCount.wordCountContainer);
+
+      document.querySelector('#editor-menu-bar').appendChild(editor.ui.view.menuBarView.element);
+
+      return editor;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
